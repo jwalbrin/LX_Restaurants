3e52fcb0af0c13b7bd6098aae68cc968dcf2931d
jwalbrin <jon.walbrin@gmail.com>
2023-08-16 15:37:29 +0100

    PrintReviewCountsPerVariable

--- Scripts/EDA/EDA.py
+++ Scripts/EDA/EDA.py
@@ -18,62 +18,105 @@ data_path = ("/home/jon/GitRepos/LX_Restaurants/Output/Formatted/")
 
 #--- Functions
 # Get indices for filter lab
-def get_filt(filt_dict, filt_lab):
+# def get_filt(filt_dict, filt_lab):
+#     """ For subsequent filtering, get: 
+#     a. Sparse binary column for a given filter label from a 
+#     dataset (e.g. "portuguese" column from Cuisine_Data.pickle)
+#     b. The row indices of all 1s from a
+#     """    
+#     # Get filt data name, find column index, get values
+#     filt_name = filt_dict["df"].columns[1].split("_")[0]
+#     targ_id = filt_dict["col_key"].index(filt_lab)
+#     filt_bin = filt_dict["df"][filt_name + "_%i" % targ_id]
+#     filt_idx = np.where(filt_bin==1)[0]
+#     print("Label and column name: %s, %s" % (filt_dict["col_key"][targ_id],
+#           filt_bin.name))
+#     return filt_bin, filt_idx
+
+def get_filt(filt_name, filt_lab, data_path):
     """ For subsequent filtering, get: 
-    a. Sparse binary column for a given filter label from a 
+    filt_name = "Cuisine"
+    filt_lab = "portuguese"
+    data_path = "/.../GitRepos/LX_Restaurants/Output/Formatted/"    
+    a. call load_bin_pickled_df for given filt_name
+    b. Sparse binary column for a given filt_lab from a 
     dataset (e.g. "portuguese" column from Cuisine_Data.pickle)
-    b. The row indices of all 1s from a
-    """    
+    c. The row indices of all 1s from b      
+    """      
+    # Load df and col keys
+    filt_path = (data_path + ("%s_Data.pickle" % filt_name))
+    filt =load_bin_pickled_df(filt_path)    
+    
     # Get filt data name, find column index, get values
-    filt_name = filt_dict["df"].columns[1].split("_")[0]
-    targ_id = filt_dict["col_key"].index(filt_lab)
-    filt_bin = filt_dict["df"][filt_name + "_%i" % targ_id]
+    targ_id = filt["col_key"].index(filt_lab)
+    filt_bin = filt["df"][filt_name + "_%i" % targ_id]
     filt_idx = np.where(filt_bin==1)[0]
-    print("Label and column name: %s, %s" % (filt_dict["col_key"][targ_id],
+    print("Label and column name: %s, %s" % (filt["col_key"][targ_id],
           filt_bin.name))
     return filt_bin, filt_idx
 
-# Load data
+# Load main data
 df = load_pickled_df(data_path + "Main_Data.pickle")
 
-#--- Filter portuguese (binary)
-filt_lab = "portuguese"
-filt_name = "Cuisine"
-
-# Load df and col keys
-filt_pickle = (data_path + ("%s_Data.pickle" % filt_name))
-filt =load_bin_pickled_df(filt_pickle)
-
-# Get filter variables
-pt_filt_b, pt_filt_i = get_filt(filt, filt_lab)
-del filt
-
-#--- Filter Price (non-nan)
-df_pr = load_pickled_df(data_path + "Price_Data.pickle")
-pr_filt_i = np.where(~np.isnan(df_pr.MidCat_3)==1)[0]
-
-# Load rev data, filter date, drop cols
+# Load all review data, trim cols
 df_rev = load_pickled_df(data_path + "Review_Data.pickle")
-df_rev = df_rev[df_rev.RevDate > "2021-01-01 00:00:00"]
 df_rev.drop(["RevTitle", "RevRating"],axis = 1,inplace = True)
 
-df_rev = df_rev[(df_rev["MainFK"].isin(pt_filt_i)) & 
-                (df_rev["MainFK"].isin(pr_filt_i))]
+#--- Show review counts for top 5 cuisines   
+top_filt_terms = ["european", "portuguese","mediterranean", 
+                  "italian", "asian"]
+tft_data = ["Cuisine"] * len(top_filt_terms)
 
+# Print length of filtered reviews
+for tft_i, tft in enumerate(top_filt_terms):
+    _, filt_i = get_filt(tft_data[tft_i], tft, data_path)
+    temp_len = len(df_rev[(df_rev["MainFK"].isin(filt_i))])
+    print("%s, %s review count: %i" % (tft_data[tft_i],tft,temp_len))
 
+# #--- Filter Price (non-nan)
+# df_pr = load_pickled_df(data_path + "Price_Data.pickle")
+# pr_filt_i = np.where(~np.isnan(df_pr.MidCat_3)==1)[0]
 
+#--- Filter AvgRating
+ar_vals = np.arange(5,0,-1)
 
-# Join price data
+# Print length of filtered reviews
+for ar_i, ar in enumerate(ar_vals):
+    filt_i = np.where(df.AvgRating==ar)[0]
+    temp_len = len(df_rev[(df_rev["MainFK"].isin(filt_i))])
+    print("%i star review count: %i" % (ar,temp_len))
+
+#--- Filter Price 
+price_vals = np.arange(2,-1,-1)
+
+# load Price data
 df_pr = load_pickled_df(data_path + "Price_Data.pickle")
 
-a = df_rev[["MainFK", "RevNum"]]
-b = df_pr[["MainFK", "MidCat_3"]]
-c = a.join(b, on = "MainFK",lsuffix='_caller', rsuffix='_other',
-           validate = "m:1")
+for pr_i, pr in enumerate(price_vals):
+    filt_i = np.where(df_pr.MidCat_3==pr)[0]
+    temp_len = len(df_rev[(df_rev["MainFK"].isin(filt_i))])
+    print("%i price cat. review count: %i" % (pr,temp_len))
 
+#--- Filter rating x price
 
+for pr_i, pr in enumerate(price_vals):
+    filt_i1 = np.where(df_pr.MidCat_3==pr)[0]
+    for ar_i, ar in enumerate(ar_vals):
+        filt_i2 = np.where(df.AvgRating==ar)[0]
+        temp_len = len(df_rev[(df_rev["MainFK"].isin(filt_i1)) &
+                              (df_rev["MainFK"].isin(filt_i2))])
+        print("%i price cat. %i star review count: %i" % (pr,ar,temp_len))
+    
 
+#--- Filter date
+date_vals = ["2023-01-01 00:00:00","2022-01-01 00:00:00",
+             "2021-01-01 00:00:00"]
 
+for d_i, d in enumerate(date_vals):
+    temp_len = len(df_rev[df_rev.RevDate > d])
+    print("Date > %s review count: %i" % (d,temp_len))
+
+    
 # #--- Frequency of unique labels
 # # Meals
 # meals = split_unique_labels(df.Meals)
